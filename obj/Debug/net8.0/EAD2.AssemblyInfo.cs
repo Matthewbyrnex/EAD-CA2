671731bf8bcc7f83abee1960b242d2c3c67455b6
Matthew Byrne <matthewb@Matthews-MacBook-Pro.local>
2024-04-25 05:10:52 +0100

    Majority of functionality with styling

--- obj/Debug/net8.0/EAD2.AssemblyInfo.cs
+++ obj/Debug/net8.0/EAD2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EAD2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbdb214031e2edc6e445e5535bb2ade0ad19381")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EAD2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EAD2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
